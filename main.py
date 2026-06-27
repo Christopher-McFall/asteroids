cd6b130610936ec6d34b1f577d1ac307d9f3251f
Christopher McFall <dutton.l.christopher@outlook.com>
2026-06-27 13:34:47 -0400

    asteroids and ship // ship controls added

--- main.py
+++ main.py
@@ -1,27 +1,49 @@
 import pygame
-from pygame import display
+from asteroid import Asteroid
 from constants import SCREEN_WIDTH
 from constants import SCREEN_HEIGHT
 from logger import log_state
-from turtle import Screen
+import player
+from player import Player
+from asteroidfield import AsteroidField
 
 
 
 
 def main():
+    # initialize pygame and create a window
     pygame.init()
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0.0
 
+    # create sprite groups and add the player to them
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+    Asteroid.containers = (updatable, drawable, asteroids)
+    AsteroidField.containers = (updatable)
+    my_player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    asteroid_field = AsteroidField()
+
+    # while loop for the game
     while True:
         log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill((0, 0, 0))
-        display.flip()    
+        updatable.update(dt)
+        for sprite in drawable:
+            sprite.draw(screen)
+        pygame.display.flip()
+        dt = clock.tick(60) / 1000
+
+        
 
 
 
